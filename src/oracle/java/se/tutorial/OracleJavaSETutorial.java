8902d08f65e7aa84d6c495e7e007a327e9494b38
Sibashis Chatterjee <sibashis1992@gmail.com>
2019-03-28 21:21:17 +0530

    Initial Commit 1

---
+++ src/oracle/java/se/tutorial/OracleJavaSETutorial.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oracle.java.se.tutorial;
+
+/**
+ *
+ * @author sibashis
+ * This project contains all the codes from Oracle Java SE Tutorial 
+ * 
+ */
+public class OracleJavaSETutorial {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        /*
+        Job 1. Any programming tutorial must start with the famous "Hello World" app.
+        */
+        
+        System.out.println ("Hello World!");
+        
+    }
+    
+}
